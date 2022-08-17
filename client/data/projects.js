f7a236a277482282c112e47889fac9f59985bced
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-18 11:37:49 +1200

    Create data file

---
+++ client/data/projects.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    id: 1,
+    title: 'The Book Exchange',
+    paragraphs: ['Final Project.', 'Bla Bla Bla.'],
+  },
+  {
+    id: 2,
+    title: 'World of Single Origin Coffee',
+    paragraphs: ['Personal Project.', 'Bla Bla Bla.'],
+  },
+  {
+    id: 3,
+    title: 'Sparkles',
+    paragraphs: ['Friday Group Project.', 'Bla Bla Bla.'],
+  },
+  {
+    id: 4,
+    title: 'My Blog Page',
+    paragraphs: ['Personal Blog.', 'Bla Bla Bla.'],
+  },
+
+  {
+    id: 5,
+    title: 'Tic Tac Toe',
+    paragraphs: ['JS Game.', 'Bla Bla Bla.'],
+  },
+]
