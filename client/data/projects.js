ae3465320685993d8a6a21e00660c4b9e6f86ec7
Daniela von Arb <103089806+danielavonarb@users.noreply.github.com>
2022-08-22 15:42:53 +1200

    Finish work entries

--- client/data/projects.js
+++ client/data/projects.js
@@ -10,7 +10,7 @@ export default [
       'bookexchange-05.png',
     ],
     title: 'The Book Exchange',
-    subtitle: 'Final Project',
+    subtitle: 'Final Dev Academy Project',
     paragraphs: [
       'For my final group project our team built a book exchange app using react-redux. It was a great experience to build a full-stack app from scratch in less than a week. I was the Vibes Watcher during this week and created a safe and fun space for the team.',
       'I am so proud of this project and the team especially! We had so much fun building this app and focused a lot on our well-being. We looked out for each other and made sure that each team member is meeting their learning objectives.',
@@ -38,46 +38,23 @@ export default [
   {
     id: 3,
     image: 'portfolio-03.jpg',
-    images: [
-      'bookexchange.png',
-      'bookexchange-02.png',
-      'bookexchange-03.png',
-      'bookexchange-04.png',
-      'bookexchange-05.png',
+    images: ['sparkles-01.png', 'sparkles-02.png'],
+    title: 'Sparkle Canvas',
+    subtitle: 'Dev Academy Group Project',
+    paragraphs: [
+      'This group project was magical! It was our first front-end react app and we had a blast building it! This project showed me what you can achieve with high levels of trust, good planning, open communication, teamwork, focus on well-being and most importantly: FUN! I made some really close friends while building this 24 hours project.',
     ],
-    title: 'Sparkles',
-    subtitle: 'Friday Group Project',
-    paragraphs: ['Bla Bla Bla.'],
     link: 'https://sparkle-canvas.herokuapp.com/',
   },
   {
     id: 4,
     image: 'portfolio-04.jpg',
-    images: [
-      'bookexchange.png',
-      'bookexchange-02.png',
-      'bookexchange-03.png',
-      'bookexchange-04.png',
-      'bookexchange-05.png',
-    ],
+    images: ['blog-01.png', 'blog-02.png', 'blog-03.png', 'blog-04.png'],
     title: 'My Blog Page',
-    subtitle: 'Personal Blog',
-    paragraphs: ['Bla Bla Bla.'],
-    link: 'https://danielavonarb.github.io/',
-  },
-
-  {
-    id: 5,
-    image: 'portfolio-05.jpg',
-    images: [
-      'bookexchange.png',
-      'bookexchange-02.png',
-      'bookexchange-03.png',
-      'bookexchange-04.png',
-      'bookexchange-05.png',
+    subtitle: 'Personal Dev Academy Blog',
+    paragraphs: [
+      'This is my first "real" website using HTML and CSS (before even knowing about JS). I was and still am so proud of this project. I felt like a developer while creating it. Little did I know of what was yet to come :-)',
     ],
-    title: 'Tic Tac Toe',
-    subtitle: 'JS Game',
-    paragraphs: ['Bla Bla Bla.'],
+    link: 'https://danielavonarb.github.io/',
   },
 ]
